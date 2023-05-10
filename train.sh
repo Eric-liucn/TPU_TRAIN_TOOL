8bc652163765b2c99559f1f184e15df7109fd233
shun <liushun0574@gmail.com>
2023-05-11 02:08:30 +0800

    not mount gcs

--- train.sh
+++ train.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-# Setup gcsfuse
+# init
 cd "$HOME" || exit
-wget https://raw.githubusercontent.com/Eric-liucn/TPU_TRAIN_TOOL/main/convert_flax_pt.py
-# gcloud init
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y fuse gcsfuse
+sudo apt-get install -y wget
 sudo apt-get install -y python3-venv
-gcsfuse -v
+wget https://raw.githubusercontent.com/Eric-liucn/TPU_TRAIN_TOOL/main/convert_flax_pt.py
 
-#gcloud auth application-default login
-mkdir "$HOME/storage"
-gcsfuse --implicit-dirs aiforsure_ai "$HOME/storage"
+export DATA_REMOTE_PATH="gs://aiforsure_ai/datasets/lady/train"
+export DATA_LOCAL_PATH="$HOME/DATA"
+
+export MODEL_REMOTE_PATH="gs://aiforsure_ai/models/basil_mix/diffusion_model_flax/*"
+export MODEL_LOCAL_PATH="$HOME/PRETRAINED_MODEL"
+
+export OUTPUT_REMOTE_PATH="gs://aiforsure_ai/train_output/text_to_img/basil_lr5e-6_30000"
+export OUTPUT_LOCAL_PT_PATH="$HOME/OUTPUT/PT"
+export OUTPUT_LOCAL_FLAX_PATH="$HOME/OUTPUT/FLAX"
+# download datasets
+gsutil -m cp -r "$DATA_REMOTE_PATH" "$DATA_LOCAL_PATH"
+# download model
+gsutil -m cp -r "$MODEL_REMOTE_PATH" "$MODEL_LOCAL_PATH"
+# setup output dir
+mkdir -p "$OUTPUT_LOCAL_PT_PATH"
+mkdir -p "$OUTPUT_LOCAL_FLAX_PATH"
 
 # Setup diffusers
+cd "$HOME" || exit
 git clone https://github.com/huggingface/diffusers.git
 cd "$HOME/diffusers" || exit
 python3 -m venv .env
@@ -28,21 +37,16 @@ pip install -r requirements_flax.txt
 
 # Run training
 cd "$HOME/diffusers/examples/text_to_image" || exit
-export MODEL_PATH="$HOME/storage/models/basil_mix/diffusion_model_flax"
-export DATA_PATH="$HOME/storage/datasets/lady"
-export OUTPUT_PATH="$HOME/storage/train_output/text_to_img/basil_lr5e-6_30000/flax"
-export OUTPUT_CONVERT_PATH="$HOME/storage/train_output/text_to_img/basil_lr5e-6_30000/pt"
 
 python train_text_to_image_flax.py \
-  --pretrained_model_name_or_path="$MODEL_PATH" \
-  --dataset_name="$DATA_PATH" \
+  --pretrained_model_name_or_path="$MODEL_LOCAL_PATH" \
+  --dataset_name="$DATA_LOCAL_PATH" \
   --resolution=512 \
   --train_batch_size=1 \
   --max_train_steps=30000 \
   --learning_rate=5e-6 \
-  --output_dir="$OUTPUT_PATH"
-
-mkdir -p "$OUTPUT_CONVERT_PATH"
+  --output_dir="$OUTPUT_LOCAL_FLAX_PATH"
 
 cd "$HOME" || exit
-python convert_flax_pt.py fp "$OUTPUT_PATH" "$OUTPUT_CONVERT_PATH"
+python convert_flax_pt.py fp "$OUTPUT_LOCAL_FLAX_PATH" "$OUTPUT_LOCAL_PT_PATH"
+gsutil -m cp -r "$HOME/OUTPUT/*" "$OUTPUT_REMOTE_PATH"
