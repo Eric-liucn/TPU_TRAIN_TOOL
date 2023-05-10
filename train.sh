e6374618747cc986ae3132adc1245563f119e231
shun <liushun0574@gmail.com>
2023-05-10 22:03:22 +0800

    1

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@
 # Setup gcsfuse
 cd "$HOME" || exit
 wget https://raw.githubusercontent.com/Eric-liucn/TPU_TRAIN_TOOL/main/convert_flax_pt.py
-#gcloud init
+# gcloud init
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
