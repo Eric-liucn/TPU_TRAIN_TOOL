70303f0bad62a241273a817d78d5df0408e4b71f
sillyL12324 <liushun0574@gmail.com>
2023-06-05 01:17:11 +0800

    comment

--- train_dream_booth.sh
+++ train_dream_booth.sh
@@ -60,6 +60,7 @@ pip install -r requirements_flax.txt
 pip install accelerate
 pip install safetensors
 pip install omegaconf
+pip install https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-2.0-cp38-cp38-linux_x86_64.whl
 accelerate config
 
 
